ef3e726168add9199482696b015102cfb3fe9ac1
Diode <148451941+Vivek210404@users.noreply.github.com>
2024-09-06 20:43:08 +0530

    Delete Lab01c.py

--- Lab01c.py
+++
@@ -1,9 +0,0 @@
-'''Write a program to take input temperature in degree celcius from user and covert into farenhiet 20/08/2024'''
-
-import math
-
-c = float(input("Enter the temperature: "))
-
-f = c*1.8+32
-
-print("Temperature in fahreinheit: ",f)
