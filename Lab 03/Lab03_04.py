3c962d686a0b4d0fd06cf4975a867387c7aad8d2
vivek210404 <vivek210404@gmail.com>
2024-09-13 22:39:59 +0530

    Adding lab codes for lab 03

--- Lab 03/Lab03_04.py
+++ Lab 03/Lab03_04.py
@@ -1,6 +1,6 @@
 n = int(input("Enter a number : "))
-i = 1
-print(f"Multiplication table of {n} : ")
-while i<=10:
+l = int(input("Enter the specified limit : "))
+
+print(f"Multiplication table of {n} upto the limit {l}: ")
+for i in range(1,l+1):
     print(f"{n} X {i} = {n*i}")
-    i=i+1
